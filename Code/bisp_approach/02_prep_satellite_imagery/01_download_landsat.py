db03712080108f9b8dec16ebf71124487e085afd
Rob Marty <ramarty@email.wm.edu>
2019-07-29 17:06:46 -0400

    code to download landsat

--- Code/bisp_approach/02_prep_satellite_imagery/01_download_landsat.py
+++ Code/bisp_approach/02_prep_satellite_imagery/01_download_landsat.py
@@ -1,7 +1,9 @@
 # https://geoscripting-wur.github.io/Earth_Engine/
 
 import ee
+import pandas as pd
 import geopandas as gpd
+import numpy as np
 import webbrowser
 import glob
 import os
@@ -10,23 +12,33 @@ import zipfile
 import re
 import geetools
 import itertools
+import math
 ee.Initialize()
 
 # Setup -----------------------------------------------------------------------
 # Filepaths
 project_file_path = '/Users/robmarty/Dropbox/World Bank/IEs/Pakistan Poverty Estimation from Satellites/'
-data_directory = project_file_path + 'Data/RawData/Landsat/unioncouncil/unstacked/'
+data_directory = project_file_path + 'Data/RawData/Landsat/bisp_households/unstacked/'
+
+bisp_coordinates_filepath = '/Users/robmarty/Desktop/'
 
 # Parameters
-uc_id = 'uc_1'
+buffer_radius = 1/111.12
 begin_date = '2016-01-01'
 end_date = '2016-12-31'
 cloud_cover_filter = 15
 resolution = 30
 
-pak_gpd = gpd.read_file(project_file_path + 'Data/FinalData/UC with NSER Data/uc_nser.geojson')
-
 # Define Useful Functions ------------------------------------------------------
+def get_lat_lon(number):
+  deg = math.floor(number / 100)
+  min = math.floor(number - (100 * deg))
+  sec = 100 * (number - (100 * deg) - min)
+  degree = deg + (min / 60) + (sec / 3600)
+
+  return(degree)
+get_lat_lon_vec = np.vectorize(get_lat_lon)  
+
 # https://stackoverflow.com/questions/35851281/python-finding-the-users-downloads-folder
 def get_download_path():
     """Returns the default downloads path for linux or windows"""
@@ -40,18 +52,31 @@ def get_download_path():
     else:
         return os.path.join(os.path.expanduser('~'), 'downloads')
 
-# Loop Through Union Councils; Extract/Download Imagery ------------------------
-for uc_id in pak_gpd['uc_id'][764:2000]:
+# Load and Prep BISP HH Coordinates ---------------------------------------------
+bisp_coords_df = pd.read_stata(bisp_coordinates_filepath + 'GPS_uid_crosswalk.dta')
+bisp_coords_df['id'] = np.arange(bisp_coords_df.shape[0]) + 1
+bisp_coords_df = bisp_coords_df.dropna()
 
-    # Pakistan Union Council i
-    pakistan = ee.FeatureCollection('users/robmarty3/uc_nser')
-    pakistan_i = pakistan.filter(ee.Filter.eq('uc_id', uc_id))
+bisp_coords_df['lat'] = get_lat_lon_vec(bisp_coords_df['GPSN'])
+bisp_coords_df['lon'] = get_lat_lon_vec(bisp_coords_df['GPSE'])
+
+# Loop Through Households; Extract/Download Imagery ------------------------
+for i in np.arange(bisp_coords_df.shape[0]):
+
+    # Create Buffer Around Household
+    bisp_coords_df_i = bisp_coords_df.iloc[i]
+    hh_id = bisp_coords_df_i['id']
+
+    hh_buffer = ee.Geometry.Rectangle([bisp_coords_df_i['lon'] - buffer_radius, 
+                                  bisp_coords_df_i['lat'] - buffer_radius, 
+                                  bisp_coords_df_i['lon'] + buffer_radius, 
+                                  bisp_coords_df_i['lat'] + buffer_radius])
 
     # Load and Filter Images By Region, Date and Cloud Cover
     image = ee.ImageCollection('LANDSAT/LE07/C01/T1_SR')
-    image = image.filterBounds(pakistan_i)
+    image = image.filterBounds(hh_buffer)
     image = image.filterDate(begin_date, end_date)
-    image = image.filterBounds(pakistan_i)
+    #image = image.filterBounds(pakistan_i)
     image = image.filter(ee.Filter.lt('CLOUD_COVER', cloud_cover_filter))
 
     # Filter by Cloud Cover
@@ -61,26 +86,17 @@ for uc_id in pak_gpd['uc_id'][764:2000]:
     image = image.reduce(ee.Reducer.median())
 
     # Make Bands Same Type, and Integer
-    image = image.multiply(1000).round().int()
+    #image = image.multiply(1000).round().int()
 
-    # Coordinates embedded differently: extract
-    try:
-        pakistan_i_coordinates = pakistan_i.geometry().getInfo()['coordinates']
-    except:
-        pakistan_i_coordinates_list = pakistan_i.geometry().getInfo()['geometries']
-        for i in range(len(pakistan_i_coordinates_list)):
-            if pakistan_i_coordinates_list[i]['type'] == 'Polygon':
-                polygon_i = i
-        pakistan_i_coordinates = pakistan_i.geometry().getInfo()['geometries'][polygon_i]['coordinates']
-
-    if(len(pakistan_i_coordinates) > 1): pakistan_i_coordinates = list(itertools.chain(*pakistan_i_coordinates))
+    # Grab Coordinates
+    hh_buffer_coordinates = hh_buffer.getInfo()['coordinates']
 
     # Export Path
-    image = image.clip(pakistan_i)
+    image = image.clip(hh_buffer)
     path = image.getDownloadUrl({
         'scale': resolution,
         'crs': 'EPSG:4326',
-        'region': pakistan_i_coordinates
+        'region': hh_buffer_coordinates
     })
 
     # Initial Files List
@@ -101,40 +117,41 @@ for uc_id in pak_gpd['uc_id'][764:2000]:
     files_after = glob.glob(get_download_path() + "/*.zip")
     zip_file = list(set(files_after) - set(files_initial))
 
-    if os.path.isdir(data_directory + uc_id) == False: os.mkdir(data_directory + uc_id)
+    hh_id = str(hh_id)
+    if os.path.isdir(data_directory + hh_id) == False: os.mkdir(data_directory + hh_id)
 
     # Extract from ZipFile in Relevant Folder
-    dir_files_initial = glob.glob(data_directory + uc_id + "/*")
+    dir_files_initial = glob.glob(data_directory + hh_id + "/*")
     zip_ref = zipfile.ZipFile(zip_file[0], 'r')
-    zip_ref.extractall(data_directory + uc_id)
+    zip_ref.extractall(data_directory + hh_id)
     zip_ref.close()
 
     # Delete tpw files
-    files_in_directory = os.listdir(data_directory + uc_id)
+    files_in_directory = os.listdir(data_directory + hh_id)
     for item in files_in_directory:
         if item.endswith(".tfw"):
-            os.remove(os.path.join(data_directory + uc_id, item))
+            os.remove(os.path.join(data_directory + hh_id, item))
 
     # Delete Uncessary Bands
-    files_in_directory = os.listdir(data_directory + uc_id)
+    files_in_directory = os.listdir(data_directory + hh_id)
     for item in files_in_directory:
         if item.endswith("mask_median.tif"):
-            os.remove(os.path.join(data_directory + uc_id, item))
+            os.remove(os.path.join(data_directory + hh_id, item))
         if item.endswith("pixel_qa_median.tif"):
-            os.remove(os.path.join(data_directory + uc_id, item))
+            os.remove(os.path.join(data_directory + hh_id, item))
         if item.endswith("radsat_qa_median.tif"):
-            os.remove(os.path.join(data_directory + uc_id, item))
+            os.remove(os.path.join(data_directory + hh_id, item))
         if item.endswith("sr_atmos_opacity_median.tif"):
-            os.remove(os.path.join(data_directory + uc_id, item))
+            os.remove(os.path.join(data_directory + hh_id, item))
         if item.endswith("sr_cloud_qa_median.tif"):
-            os.remove(os.path.join(data_directory + uc_id, item))
+            os.remove(os.path.join(data_directory + hh_id, item))
 
     # Rename tif files
-    files_in_directory = os.listdir(data_directory + uc_id)
+    files_in_directory = os.listdir(data_directory + hh_id)
     for orig_name in files_in_directory:
         new_name = 'B'+ re.sub(".*B","",orig_name)
-        os.rename(data_directory + uc_id + "/" + orig_name, data_directory + uc_id + "/" + new_name)
+        os.rename(data_directory + hh_id + "/" + orig_name, data_directory + hh_id + "/" + new_name)
 
     # Delete Zip File
     os.remove(zip_file[0])
-    time.sleep(5)
+    time.sleep(0.2)

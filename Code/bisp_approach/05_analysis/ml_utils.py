20869a70d62cb7cfd1959cb16286249ebfe77790
Rob Marty <ramarty@email.wm.edu>
2019-11-10 20:05:29 -0500

    predict changes

---
+++ Code/bisp_approach/05_analysis/ml_utils.py
@@ -0,0 +1,16 @@
+# ml_utils.py
+#
+# Description:
+# Defines useful functions and classes used in the ML pipeline.
+
+# Define a TrainedRegressor object to hold key results information
+class TrainedRegressor:
+
+    def __init__(self, method, params, features, regressor):
+        self.method = method
+        self.params = params
+        self.regressor = regressor
+        self.features = features
+
+    def __repr__(self):
+        return f'Trained {self.method} on feature set {self.features} with params {self.params}'

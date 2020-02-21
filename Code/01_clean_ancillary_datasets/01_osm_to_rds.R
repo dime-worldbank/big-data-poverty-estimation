e8fc048ae0f0a7dce4d9dccd1b420965ed8dcfa6
Rob Marty <ramarty@email.wm.edu>
2020-02-21 13:36:15 -0500

    update osm extraction

--- Code/01_clean_ancillary_datasets/01_osm_to_rds.R
+++ Code/01_clean_ancillary_datasets/01_osm_to_rds.R
@@ -9,7 +9,7 @@ files <- list.files(file.path(project_file_path, "Data", "RawData", "OSM"), patt
 for(file_i in files){
   print(file_i)
   data_sdf <- readOGR(dsn = file.path(project_file_path, "Data", "RawData", "OSM"), layer=file_i)
-  saveRDS(file.path(project_file_path, "Data", "FinalData", "OSM", paste0(file_i, ".Rds")))
+  saveRDS(data_sdf, file.path(project_file_path, "Data", "FinalData", "OSM", paste0(file_i, ".Rds")))
 }
 
 

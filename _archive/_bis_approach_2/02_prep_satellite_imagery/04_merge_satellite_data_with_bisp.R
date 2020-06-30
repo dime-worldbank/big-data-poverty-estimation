3a45455b4bc1c6eef3ea1646f1ceeef4a255521b
Rob Marty <ramarty@email.wm.edu>
2020-06-29 23:28:54 -0400

    reorg

--- _archive/_bis_approach_2/02_prep_satellite_imagery/04_merge_satellite_data_with_bisp.R
+++
@@ -1,9 +0,0 @@
-# Merge Satellite Data with BISP Survey Data
-
-# Load Data --------------------------------------------------------------------
-landsat <- raster(file.path(rawdata_file_path, "Landsat", "bisp_households", "unstacked", "17", "B1_median.tif"))
-plot(landsat)
-
-landsat[]
-
-coordinates(landsat)
--- _archive/nser_approach/02_merge_survey_with_shapefile/04_create_final_geolocations.R
+++
@@ -1,9 +0,0 @@
-# Clean NSER Survey
-
-# Load and Clean Data ----------------------------------------------------------
-survey_df <- readRDS(file.path(final_data_file_path, "UC with NSER Data", "individual_files", "nsersurvey_withdata.Rds"))
-survey_hdx_shp_names <- readRDS(file.path(final_data_file_path, "UC with NSER Data", "individual_files", "nsersurvey_matched_hdx_shapefile.Rds"))
-survey_googlecoords <- readRDS(file.path(final_data_file_path, "UC with NSER Data", "individual_files", "nsersurvey_matched_google_coords.Rds"))
-
-survey_df <- merge(survey_df, survey_hdx_shp_names, by="id", all=T)
-survey_df <- merge(survey_df, survey_googlecoords, by="id", all=T)

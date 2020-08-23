25633b80a0a3a9879dcd26102b92260df6c1a393
nluong0 <64280847+nluong0@users.noreply.github.com>
2020-08-22 21:20:55 -0500

    Reproducibility tested

--- DataWork/03_analysis/poverty_estimation/clean_data.py
+++ DataWork/03_analysis/poverty_estimation/clean_data.py
@@ -26,7 +26,6 @@ def load_data():
     '''
     df = pd.read_csv(DATA_FILEPATH)
     df = df[df['year'] == 2014]
-    
     return df
 
 
@@ -36,7 +35,6 @@ def transform_target(df):
     '''
     df[TARGET_NAME] = (df['pscores']<=16.17)
     df[TARGET_NAME] = df[TARGET_NAME].astype(int)
-    
     return df
 
 
@@ -48,7 +46,6 @@ def get_lat_lon(number):
     min = floor(number - (100 * deg))
     sec = 100 * (number - (100 * deg) - min)
     degree = deg + (min / 60) + (sec / 3600)
-    
     return degree
 
 
@@ -67,7 +64,6 @@ def load_and_prep_coords():
     coords['uid'] = coords['uid'].astype(int)
     # Create geopandas
     gdf = gpd.GeoDataFrame(coords, geometry=gpd.points_from_xy(coords['lon'], coords['lat']))
-    
     return gdf
 
 
@@ -79,50 +75,41 @@ def load_and_clean_data():
     # LOAD BISP DATA AND TRANSFORM TARGET
     df = load_data()
     df = transform_target(df)
-    print(f'{datetime.datetime.now()}   1.1 BISP data loaded and target transformed.')
+    print(f'{datetime.datetime.now()}    1.1 BISP data loaded and target transformed.')
 
-    # LOAD BISP DATA COORDINATES 
+    # LOAD AND MATCH COORDS TO BISP DATA
     coords = load_and_prep_coords()
-
-    # MATCH COORDS TO BISP DATA
     gdf_bisp = coords.merge(df, left_on='uid', right_on='uid')
     gdf_bisp.to_pickle('bisp_with_coords.pkl')
-    print(f'{datetime.datetime.now()}   1.2 Coordinates added to BISP data.')
-    
-    # LOAD SATELLITE DATA
+    print(f'{datetime.datetime.now()}    1.2 Coordinates added to BISP data.')
+
+    # LOAD AND SPATIALLY JOIN SATELLITE DATA TO BISP DATA
     viirs = pd.read_pickle(VIIRS_GDF_FILEPATH)
     viirs_gdf = gpd.GeoDataFrame(viirs, geometry='geometry')
-    print(f'{datetime.datetime.now()}   1.3 Satellite data loaded.')
-
-    # SPATIALLY JOIN SATELLITE AND BISP DATA
     gdf = gpd.sjoin(viirs_gdf, gdf_bisp, how="inner", op='intersects').reset_index(drop=True)
-    print(f'{datetime.datetime.now()}   1.4 Satellite and BISP data spatially joined.')
+    print(f'{datetime.datetime.now()}    1.3 Satellite and BISP data spatially joined.')
 
     # MAP DTL IMAGE FILES TO BISP DATA
-    DTL, processed_gdf = fe.map_DTL_NTL(gdf, DTL_DIRECTORY)
-    print(f'{datetime.datetime.now()}   1.5 DTL images mapped to BISP data.')
-
-    # RESHAPE DTL
     h, w, c = 25, 26, 7
+    DTL, processed_gdf = fe.map_DTL_NTL(gdf, DTL_DIRECTORY)
     DTL = DTL.reshape((DTL.shape[0], h, w, c))
+    print(f'{datetime.datetime.now()}    1.4 DTL images mapped to BISP data.')
 
     # DEFINE FEATURE GROUPS AND FINAL DATA
     df_viirs = processed_gdf.filter(regex='viirs').filter(regex='_2km')
     df_landsat = processed_gdf.filter(regex='^b').filter(regex='_1km')
     df_osm = processed_gdf.filter(regex='fclass').filter(regex='meters')
-    df_facebook = processed_gdf.filter(regex='^estimate_dau')
+    df_fb = processed_gdf.filter(regex='^estimate_dau')
     df_y = processed_gdf[['uid', TARGET_NAME]]
-    df_final = df_y.join(df_osm).join(df_facebook).join(df_landsat).join(df_viirs).reset_index(drop=True)
-    df_x = df_final.drop(columns=['uid', TARGET_NAME])
+    df_final = df_y.join(df_osm).join(df_fb).join(df_landsat).join(df_viirs).reset_index(drop=True)
 
     # STORE FEATURE GROUPS IN DICT
     geo_features = df_viirs.columns.tolist() + df_landsat.columns.tolist()
-    osm_fb_features = df_osm.columns.tolist() + df_facebook.columns.tolist()
+    osm_fb_features = df_osm.columns.tolist() + df_fb.columns.tolist()
 
-    feature_dict = {'ALL_FEATURES': df_x.columns.tolist(),
-                    'NUMERIC_GEO_FEATURES': geo_features,
+    feature_dict = {'NUMERIC_GEO_FEATURES': geo_features,
                     'OSM_FB_FEATURES': osm_fb_features}
-    print(f'{datetime.datetime.now()}   1.6 Final data and features groups defined.')
+    print(f'{datetime.datetime.now()}    1.5 Final data and features groups defined.')
 
     return df_final, feature_dict, DTL
 
--- DataWork/03_analysis/poverty_estimation/predict_visualize.py
+++ DataWork/03_analysis/poverty_estimation/predict_visualize.py
@@ -37,28 +37,30 @@ PAKISTAN_BOUNDARIES_SHAPEFILE = cf.PAKISTAN_BOUNDARIES_SHAPEFILE
 
 
 def get_best_model():
+    '''
+    Grabs best model according to grid search.
+    '''
     # Load grid search results
     results = pd.read_csv(RESULTS)
     results_sorted = results.sort_values(['final_score', 'accuracy_score'], ascending=False)
-
+    
     # Load best regressor by accuracy
     df_bestmodel = results_sorted.iloc[0]
     best_regressor = pd.read_pickle(f'output/{df_bestmodel["regressor"]}_trained.pkl')
-
+    
     # Grab best model
     best_model = None
     for x in best_regressor:
         if df_bestmodel['params'] in x.params and df_bestmodel['feature_group'] in x.features:
             best_model = x
-
-    print(f'BEST MODEL: {best_model}')
-    print(f'FINAL SCORE: {df_bestmodel["final_score"]}')
-    print(f'FEATURE_GROUP: {df_bestmodel["feature_group"]}')
     
     return df_bestmodel, best_model
 
 
 def get_predictions(feature_columns, best_model):
+    '''
+    Predicts binary poverty for entire dataset.
+    '''
     # Merge manipulated bisp data and fully prepped data to get geometry for predictions
     original = pd.read_pickle(BISP_WITH_COORDS)[['uid', 'geometry']]
     original = gpd.GeoDataFrame(original, geometry='geometry')
@@ -187,15 +189,20 @@ def main():
 
     # GET BEST MODEL
     df_bestmodel, best_model = get_best_model()
+    print(f'Best Model: {best_model.method}')
+    print(f'Feature Group: {best_model.features}')
+    print(f'Parameters: {best_model.params}')
+    print(f'Accuracy: {df_bestmodel["accuracy_score"]}')
+    print(f'Precision: {df_bestmodel["precision_score"]}')
+    print(f'Recall for HHs in Poverty: {df_bestmodel["recall_poverty"]}')
+    print(f'Recall for HHs Not in Poverty: {df_bestmodel["recall_nonpoverty"]}')
 
     # GET PREDICTIONS
     feature_columns = ast.literal_eval(df_bestmodel["feature_columns"])
     df, true_labels, pred_labels, column_names = get_predictions(feature_columns, best_model)
-    print(type(df))
-    print(type(df[df['true_label']==0]))
 
     # GENERATE AND SAVE VISUALIZATIONS
-    #fig1_eval_metrics(df_bestmodel, true_labels, pred_labels)
+    fig1_eval_metrics(df_bestmodel, true_labels, pred_labels)
     fig2_confusion_matrix(true_labels, pred_labels)
     fig3_feat_importances(best_model, column_names)
     fig4_map(df)

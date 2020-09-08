f6dee5ef9567c55dec6eb0fc9df7ca58ba6ee6ee
Rob Marty <ramarty@email.wm.edu>
2020-09-08 18:27:27 -0400

    extract features from cnn

--- DataWork/03_analysis/poverty_estimation/03_extract_cnn_features_to_bisp.py
+++ DataWork/03_analysis/poverty_estimation/03_extract_cnn_features_to_bisp.py
@@ -4,6 +4,9 @@
 # Performs complete process of cleaning data and prepping all necessary data 
 # and running the grid search.
 
+# https://towardsdatascience.com/extract-features-visualize-filters-and-feature-maps-in-vgg16-and-vgg19-cnn-models-d2da6333edd0
+
+
 import os, math, pickle, datetime, json
 import numpy as np
 import pandas as pd
@@ -68,195 +71,32 @@ def normalize(x_train, x_test):
     for df in (x_train, x_test):
         x_scaler.transform(df)
 
-
-
-def extract_cnn_data():
+def extract_features_to_pd():
 
     # 1. Load Data
     DTL = np.load(os.path.join(cf.DROPBOX_DIRECTORY, 'Data', 'BISP' , 'FinalData', 'Individual Datasets', 'bisp_dtl.npy'))
     bisp_df = pd.read_pickle(os.path.join(cf.DROPBOX_DIRECTORY, 'Data', 'BISP' , 'FinalData', 'Individual Datasets', 'bisp_dtl_uids.pkl'))
 
     # 2. Extract features
-    # https://towardsdatascience.com/extract-features-visualize-filters-and-feature-maps-in-vgg16-and-vgg19-cnn-models-d2da6333edd0
-
-    layer_name = 'dense1'
+    layer_name = 'fc1'
     model = load_model(cf.CNN_FILENAME)
 
-    #df_features = fe.extract_features(model, DTL, layer_name)
+    DTL_p = preprocess_input(DTL) # Preprocess image data
 
-
-
-
-    # Preprocess image data
-    DTL_p = preprocess_input(DTL)
-
-
+    #DTL_p = DTL_p[1:5,:,:,:] # for testing
 
     # Generate feature extractor using trained CNN
     feature_extractor = Model(inputs=model.inputs,
                               outputs=model.get_layer(name=layer_name).output,)
 
+    features = feature_extractor.predict(DTL_p)
 
-
-    a = feature_extractor.predict(DTL_p)
-                              
-
-    # Extract features and convert from tensor to numpy array
-    # features = feature_extractor(DTL_p).numpy()
-    features = feature_extractor(DTL_p)
-
-    # Create and format pandas DataFrame
-    df = pd.DataFrame(features).add_prefix('feat_')
-
-
-
-
-    # USE PCA TO SELECT EXTRACTED FEATURES
-    print(f'{datetime.datetime.now()} 3. Performing PCA.')
-    df_features_pca = perform_pca(df_features, 10)
-    feature_dict['EXTRACTED_FEATURES'] = df_features_pca.columns.to_list()
-    feature_dict['EXTRACT_OSM_FB_FEATURES'] = feature_dict['EXTRACTED_FEATURES'] \
-                                        + feature_dict['OSM_FB_FEATURES']
-
-
-
-
-    ############# DONE ############
-
-
-
-
-
-
-    
-
-    # 1. Load Grid for DTL Tiles
-    dtl_tiles = gpd.read_file(os.path.join(cf.DROPBOX_DIRECTORY, 'Data', 'Pakistan Grid', 'RawData', 'pak_grid_200km.geojson'))
-    dtl_tiles.rename(columns = {'id': 'tile_id'}, inplace=True)
-
-    # 2. Prep BISP data
-    # Load, convert to geopandas, extract dtl tile
-    bisp_df = pd.read_csv(os.path.join(cf.SECURE_DATA_DIRECTORY, 'Data', 'BISP', 'FinalData - PII', 'GPS_uid_crosswalk.csv'))
-    bisp_gdf = pd_to_gdp(bisp_df)
-    bisp_gdf = gpd.sjoin(bisp_gdf, dtl_tiles, how="inner", op='intersects').reset_index(drop=True)
-    bisp_gdf['geometry'] = bisp_gdf.buffer(distance = 0.75/111.12).envelope
-    
-    # 3. Extract DTL to BISP Coordinates
-
-    # Load CNN parameters 
-    with open(cf.CNN_PARAMS_FILENAME, 'r') as fp:
-        cnn_param_dict = json.load(fp)
-
-    # Extract
-    bisp_gdf_s = bisp_gdf.head(10)
-    import feature_extraction as fe
-    DTL, bisp_gdf_processed = fe.map_DTL_NTL(input_gdf = bisp_gdf_s, 
-                                        directory = cf.DTL_DIRECTORY, 
-                                        bands = cnn_param_dict['bands'], 
-                                        img_height = cnn_param_dict['image_height'], 
-                                        img_width = cnn_param_dict['image_width'])
-
-
-
-     ## DONE!   
-
-
-    out_img = DTL[1,2]
-    show(out_img)
-
-    # Remove border b/c of weird zero issue?
-    out_img = np.delete(out_img,  1, axis=1)
-    out_img = np.delete(out_img, -1, axis=1)
-    out_img = np.delete(out_img, -1, axis=2)
-    out_img = np.delete(out_img, -1, axis=2)
-
-
-
-
-    h, w, c = 25, 26, 7
-    DTL, processed_gdf = fe.map_DTL_NTL(gdf, DTL_DIRECTORY)
-    DTL = DTL.reshape((DTL.shape[0], h, w, c)) 
-
-
-
-
-    #### Load Data
-    # 1. BISP Coordiantes
-
-
-
-
-
-
-
-
-
-    df, feature_dict, DTL = load_and_clean_data()
-
-
-
-    
-
-    layer_name = 'dense1'
-    model = load_model(cnn_filepath)
-    df_features = extract_features(model, DTL, layer_name)
-
-
-
-    #################
-
-
-    # SET DIRECTORY
-    os.chdir(CURRENT_DIRECTORY)
-    print(f'{datetime.datetime.now()} RUNNING GRID SEARCH')
-
-    # if not grid_ready_data:
-    # LOAD CLEANED DATA, DICT OF FEATURE GROUPS, AND DTL
-    print(f'{datetime.datetime.now()} 1. Loading/Cleaning Data.')
-    df, feature_dict, DTL = load_and_clean_data()
-
-    # LOAD CNN, EXTRACT FEATURES, ADD TO FEATURE GROUPS
-    print(f'{datetime.datetime.now()} 2. Extracting Features.')
-    layer_name = 'dense1'
-    model = load_model(cnn_filepath)
-    df_features = extract_features(model, DTL, layer_name)
-
-    # USE PCA TO SELECT EXTRACTED FEATURES
-    print(f'{datetime.datetime.now()} 3. Performing PCA.')
-    df_features_pca = perform_pca(df_features, 10)
-    feature_dict['EXTRACTED_FEATURES'] = df_features_pca.columns.to_list()
-    feature_dict['EXTRACT_OSM_FB_FEATURES'] = feature_dict['EXTRACTED_FEATURES'] \
-                                        + feature_dict['OSM_FB_FEATURES']
-
-    # DEFINE FINAL DATA
-    df_final = df.join(df_features_pca)
-    df_final.to_pickle('fully_prepped_data.pkl')
-
-    # OVERSAMPLE
-    print(f'{datetime.datetime.now()} 4. Defining Sample.')
-    x_df = pd.DataFrame(df_final.drop(labels=['uid', TARGET_NAME], axis=1))
-    y_df = df_final[TARGET_NAME]
-    feature_dict['ALL_FEATURES'] = x_df.columns.tolist()
-    oversample = RandomOverSampler(sampling_strategy=0.65)
-    x, y = oversample.fit_resample(x_df, y_df)
-
-    # SPLIT INTO TRAIN/TEST AND NORMALIZE
-    print(f'{datetime.datetime.now()} 5. Defining Training and Testing Sets.')
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SIZE)
-    normalize(x_train, x_test)
-
-    # TRAIN MODELS AND EXPORT ERRORS
-    print(f'{datetime.datetime.now()} 6. Training Models.')
-    parameters = cf.GRID_TEST_CLASS
-    training_errors = train_models(parameters, x_train, y_train, feature_dict)
-    training_errors.to_csv(os.path.join('output', 'errors.csv'))
-
-    # PREDICT LABELS AND EVALUATE RESULTS
-    print(f'{datetime.datetime.now()} 7. Evalating Models.')
-    trained_obj_list = [f for f in os.listdir('output') if f.endswith('_trained.pkl')]
-    results_df = evaluate_models(trained_obj_list, x_test, y_test, feature_dict)
-    results_df.to_csv(os.path.join('output', 'results.csv')) 
-
+    # 3. Create and format pandas DataFrame
+    df = pd.DataFrame(features).add_prefix('cnn_feat_')
+    df['uid'] = bisp_df.uid
+               
+    # 4. Export           
+    df.to_pickle(os.path.join(cf.DROPBOX_DIRECTORY, 'Data', 'BISP' , 'FinalData', 'Individual Datasets', 'bisp_cnn_features_all.pkl'))
 
 if __name__ == '__main__':
-    run_grid_search()
+    extract_features_to_pd()

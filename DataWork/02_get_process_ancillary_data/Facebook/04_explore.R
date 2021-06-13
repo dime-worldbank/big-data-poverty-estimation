c7b7e8f19b4e2d4181563a4fc67e7ddff6fd47c6
Rob Marty <ramarty@email.wm.edu>
2021-06-12 20:32:16 -0400

    fb scraping

--- DataWork/02_get_process_ancillary_data/Facebook/04_explore.R
+++ DataWork/02_get_process_ancillary_data/Facebook/04_explore.R
@@ -1,17 +1,42 @@
 # Merge Data Extracted from Facebook API
 
 # Load Data --------------------------------------------------------------------
-opm_pov <- readRDS(file.path(data_dir, "OPM", "FinalData", "Individual Datasets", "bisp_socioeconomic.Rds"))
-fb_prop_df <- readRDS(file.path(data_dir, "OPM", "FinalData", "Individual Datasets", "facebook_marketing_dau_mau_prop.Rds"))
+SURVEY_NAME <- "DHS"
 
-opm_pov$uid <- opm_pov$uid %>% as.numeric()
+survey_pov <- readRDS(file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "survey_socioeconomic.Rds"))
+fb_df <- readRDS(file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau.Rds"))
+fb_prop_df <- readRDS(file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau_prop.Rds"))
+params_df <- readRDS(file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_parameters.Rds"))
 
-opm_pov <- opm_pov %>%
+fb_prop_df <- fb_prop_df %>% 
+  rename_at(vars(-uid), ~ paste0(., '_prop'))
+
+survey_pov <- survey_pov %>%
+  left_join(fb_df, by = "uid") %>%
   left_join(fb_prop_df, by = "uid") %>%
-  dplyr::filter(!is.na(estimate_dau_20210414040732_2)) %>%
-  dplyr::filter(year == 2014) %>%
-  group_by(cluster_id) %>%
-  summarise_if(is.numeric, mean) 
+  dplyr::filter(!is.na(estimate_mau_1))
+
+survey_pov_long <- survey_pov %>%
+  dplyr::select(uid, wealth_index, wealth_index_score, urban_rural, contains("estimate_")) %>%
+  pivot_longer(cols = -c(uid, wealth_index, wealth_index_score, urban_rural)) %>%
+  dplyr::rename(fb_value = value,
+                fb_variable = name)
+
+cor_df <- survey_pov_long %>%
+  group_by(fb_variable) %>%
+  dplyr::summarise(cor_index = cor(wealth_index, fb_value),
+                   cor_score = cor(wealth_index_score, fb_value)) %>%
+  dplyr::filter(fb_variable %>% str_detect("mau"))
+
+survey_pov %>%
+  ggplot() +
+  geom_point(aes(x = estimate_mau_18_prop,
+                 y = wealth_index_score))
+
+
+
+
+
 
 # opm_pov <- opm_pov %>%
 #   dplyr::select(cluster_id, contains("mau"), pscores) %>%

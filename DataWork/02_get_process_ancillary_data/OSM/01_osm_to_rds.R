b7b33f59a57166e4b0e2f565088a064753f0e43b
Rob Marty <ramarty@email.wm.edu>
2021-08-11 19:45:45 -0400

    process osm

--- DataWork/02_get_process_ancillary_data/OSM/01_osm_to_rds.R
+++ DataWork/02_get_process_ancillary_data/OSM/01_osm_to_rds.R
@@ -8,7 +8,9 @@
 OVERWRITE_FILES <- F
 
 ## Loop through country-year directories
-country_year_dirs <- list.files(file.path(project_file_path, "Data", "OSM", "RawData"), pattern=".shp") 
+country_year_dirs <- list.files(file.path(project_file_path, "Data", "OSM", "RawData")) %>% 
+  str_replace_all(".shp", "") %>%
+  str_subset("-free")
 
 country_year_dirs <- country_year_dirs %>% str_subset("nepal")
 
@@ -16,11 +18,11 @@ for(dir_i in country_year_dirs){
   print(paste(dir_i, "---------------------------------------------------------"))
   
   ## Create final Data Directory
-  finaldata_dir <- file.path(project_file_path, "Data", "OSM", "FinalData", dir_i %>% str_replace_all(".shp", ""))
+  finaldata_dir <- file.path(project_file_path, "Data", "OSM", "FinalData", dir_i)
   dir.create(finaldata_dir)
   
   ## Loop through files; load raw data, save as Rds
-  files <- list.files(file.path(project_file_path, "Data", "OSM", "RawData", dir_i), pattern=".shp") %>% str_replace_all(".shp", "")
+  files <- list.files(file.path(project_file_path, "Data", "OSM", "RawData", paste0(dir_i, ".shp")), pattern=".shp") %>% str_replace_all(".shp", "")
   
   for(file_i in files){
     print(file_i)
@@ -28,7 +30,7 @@ for(dir_i in country_year_dirs){
     OUT_PATH <- file.path(finaldata_dir, paste0(file_i, ".Rds"))
     
     if(!file.exists(OUT_PATH) | OVERWRITE_FILES){
-      data_sdf <- readOGR(dsn = file.path(project_file_path, "Data", "OSM", "RawData", dir_i), layer = file_i)
+      data_sdf <- readOGR(dsn = file.path(project_file_path, "Data", "OSM", "RawData", paste0(dir_i, ".shp")), layer = file_i)
       saveRDS(data_sdf, OUT_PATH)
     }
     

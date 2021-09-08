31da886623426fd1668b6f90556b2a6d5c8753b0
Rob Marty <ramarty@email.wm.edu>
2021-09-08 18:10:56 -0400

    update gadm list

--- DataWork/00_download_gadm/download_gadm.R
+++ DataWork/00_download_gadm/download_gadm.R
@@ -2,7 +2,13 @@
 
 setwd(file.path(data_dir, "GADM", "RawData"))
 
-for(country_i in c("BGD", "IND", "KHM", "KGZ", "MMR", "NGA", "NPL", "PHL", "PAK", "SEN", "TJK", "TLS", "UGA")){
+for(country_i in c("ALB", "ARM", "AGO", "BFA", "BEN", "BOL", "BDI", "COD", "CAF",
+                   "CIV", "CMR", "COL", "DOM", "EGY", "ETH", "GAB", "GHA", "GMB",
+                   "GIN", "GTM", "GUY", "HND", "HTI", "JOR", "KEN", "COM", "LBR",
+                   "LSO", "MAR", "MDA", "MDG", "MLI", "MWI", "MOZ", "NGA", "NER",
+                   "NAM", "PER", "RWA", "SLE", "SEN", "TCD", "TGO", "TZA", "ZAF",
+                   "ZMB", "ZWE",
+                   "BGD", "IND", "KHM", "KGZ", "MMR", "NGA", "NPL", "PHL", "PAK", "TJK", "TLS", "UGA")){
   for(i in 0:2){
     getData('GADM', country=country_i, level=i)
   } 

04dbbafc46d1324c84d85db375842faeeb2544bc
Rob Marty <ramarty@email.wm.edu>
2021-09-11 14:01:47 -0400

    update

--- DataWork/02_get_process_ancillary_data/OSM/02_split_to_unique_countries.R
+++ DataWork/02_get_process_ancillary_data/OSM/02_split_to_unique_countries.R
@@ -9,7 +9,9 @@ split_files_to_new_dirs <- function(dirs_vec,
                                     new_name_1,
                                     new_name_2,
                                     iso_1,
-                                    iso_2){
+                                    iso_2,
+                                    extract_1 = TRUE,
+                                    extract_2 = TRUE){
   # DESCRIPTION: Splits files by country into new country-specific directories
   # ARGS:
   # dirs_vec: Vector of names of all OSM directories with country pairs
@@ -18,7 +20,10 @@ split_files_to_new_dirs <- function(dirs_vec,
   # new_name_2: What to rename directory for country 2 (eg, "haiti")
   # iso_1: iso3 of country 1
   # iso_2: iso3 of country 2
+  # extract_1: If want to create new directory+files for country 1
+  # extract_2: If want to create new directory+files for country 2
   
+  ## Loop through directories 
   for(dir_i in dirs_vec){
     print(dir_i)
     
@@ -27,36 +32,48 @@ split_files_to_new_dirs <- function(dirs_vec,
     dir_1_i <- dir_i %>% str_replace_all(root_name_orig, new_name_1)
     dir_2_i <- dir_i %>% str_replace_all(root_name_orig, new_name_2)
     
-    dir.create(file.path(osm_dir, "FinalData", dir_1_i))
-    dir.create(file.path(osm_dir, "FinalData", dir_2_i))
+    if(extract_1 %in% TRUE) dir.create(file.path(osm_dir, "FinalData", dir_1_i))
+    if(extract_2 %in% TRUE) dir.create(file.path(osm_dir, "FinalData", dir_2_i))
     
+    ## Loop through files within directory i
     for(file_i in files){
       print(file_i)
       df_i <- readRDS(file.path(osm_dir, "FinalData", dir_i, file_i))
       
       ## Load country files
-      gadm_1 <- readRDS(file.path(gadm_dir, "RawData", paste0("gadm36_",iso_1,"_0_sp.rds")))
-      gadm_2 <- readRDS(file.path(gadm_dir, "RawData", paste0("gadm36_",iso_2,"_0_sp.rds")))
+      if(extract_1 %in% TRUE){
+        ## Load GADM
+        gadm_1 <- readRDS(file.path(gadm_dir, "RawData", paste0("gadm36_",iso_1,"_0_sp.rds")))
+        
+        ## Extent files 
+        gadm_1_extent <- as(extent(gadm_1), "SpatialPolygons")
+        crs(gadm_1_extent) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+        gadm_1_extent$id <- 1
+        
+        ## Intersects
+        df_1_i <- df_i[gIntersects_chunks(df_i, gadm_1_extent, 100),]
+        df_1_i <- df_1_i[gIntersects_chunks(df_1_i, gadm_1, 50),]
+        
+        ## Export
+        saveRDS(df_1_i, file.path(osm_dir, "FinalData", dir_1_i, file_i))
+      } 
       
-      ## Extent files 
-      gadm_1_extent <- as(extent(gadm_1), "SpatialPolygons")
-      crs(gadm_1_extent) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-      gadm_1_extent$id <- 1
-      
-      gadm_2_extent <- as(extent(gadm_2), "SpatialPolygons")
-      crs(gadm_2_extent) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-      gadm_2_extent$id <- 1
-      
-      ## Intersects
-      df_1_i <- df_i[gIntersects_chunks(df_i, gadm_1_extent, 100),]
-      df_1_i <- df_1_i[gIntersects_chunks(df_1_i, gadm_1, 50),]
-      
-      df_2_i <- df_i[gIntersects_chunks(df_i, gadm_2_extent, 100),]
-      df_2_i <- df_2_i[gIntersects_chunks(df_2_i, gadm_2, 50),]
-      
-      ## Export
-      saveRDS(df_1_i, file.path(osm_dir, "FinalData", dir_1_i, file_i))
-      saveRDS(df_2_i, file.path(osm_dir, "FinalData", dir_2_i, file_i))
+      if(extract_2 %in% TRUE){ 
+        ## Load GADM
+        gadm_2 <- readRDS(file.path(gadm_dir, "RawData", paste0("gadm36_",iso_2,"_0_sp.rds")))
+        
+        ## Extent files 
+        gadm_2_extent <- as(extent(gadm_2), "SpatialPolygons")
+        crs(gadm_2_extent) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+        gadm_2_extent$id <- 1
+        
+        ## Intersects
+        df_2_i <- df_i[gIntersects_chunks(df_i, gadm_2_extent, 100),]
+        df_2_i <- df_2_i[gIntersects_chunks(df_2_i, gadm_2, 50),]
+        
+        ## Export
+        saveRDS(df_2_i, file.path(osm_dir, "FinalData", dir_2_i, file_i))
+      }
     }
   }
   
@@ -64,14 +81,44 @@ split_files_to_new_dirs <- function(dirs_vec,
 }
 
 ## Haiti and Dominican Republic
-haiti_domrep_dirs <- file.path(osm_dir, "FinalData") %>%
+dom_hti_dirs <- file.path(osm_dir, "FinalData") %>%
   list.files() %>%
   str_subset("haiti-and-domrep")
 
-split_files_to_new_dirs(dirs_vec = haiti_domrep_dirs,
+split_files_to_new_dirs(dirs_vec = dom_hti_dirs,
                         root_name_orig = "haiti-and-domrep",
                         new_name_1 = "domrep",
                         new_name_2 = "haiti",
                         iso_1 = "DOM",
                         iso_2 = "HTI")
+
+## Haiti and Dominican Republic
+sen_gam_dirs <- file.path(osm_dir, "FinalData") %>%
+  list.files() %>%
+  str_subset("senegal-and-gambia")
+
+split_files_to_new_dirs(dirs_vec = sen_gam_dirs,
+                        root_name_orig = "senegal-and-gambia",
+                        new_name_1 = "senegal",
+                        new_name_2 = "gambia",
+                        iso_1 = "SEN",
+                        iso_2 = "GMB")
+
+## Timor-Leste (Extracted from Indonesia)
+sen_gam_dirs <- file.path(osm_dir, "FinalData") %>%
+  list.files() %>%
+  str_subset("senegal-and-gambia")
+
+split_files_to_new_dirs(dirs_vec = sen_gam_dirs,
+                        root_name_orig = "indonesia",
+                        new_name_1 = "timor-leste",
+                        new_name_2 = "indonesia_TEMP",
+                        iso_1 = "TLS",
+                        iso_2 = "IDN",
+                        extract_1 = TRUE,
+                        extract_2 = FALSE)
+
+
+
+
 

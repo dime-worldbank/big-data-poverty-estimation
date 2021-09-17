a42b7ceb580f2fb79b954bc61ae13f26c8dbe742
Rob Marty <ramarty@email.wm.edu>
2021-09-17 10:28:13 -0400

    osm distance

--- DataWork/02_get_process_ancillary_data/OSM/02_split_up_large_files.R
+++ DataWork/02_get_process_ancillary_data/OSM/02_split_up_large_files.R
@@ -3,23 +3,43 @@
 # India OSM file is large, so split up to facilitate faster processing
 
 # Load data --------------------------------------------------------------------
-osm_country_dir <- "india-210101-free"
-osm_df <- readRDS(file.path(osm_dir, "FinalData", 
-                            osm_country_dir, "gis_osm_roads_free_1.Rds"))
-
-N_SUBSETS <- 30
-
-group_vec <- rep_len(x = 1:N_SUBSETS, length.out = nrow(osm_df))
-
-for(i in 1:N_SUBSETS){
-  print(i)
+for(osm_country_dir in c("india-210101-free")){
+  print(osm_country_dir)
   
-  osm_df_i <- osm_df[group_vec %in% i,]
+  if(osm_country_dir %in% "india-210101-free") N_SUBSETS <- 50
   
-  saveRDS(osm_df_i,
-          file.path(osm_dir, "FinalData", 
-                    osm_country_dir, paste0("gis_osm_roads_free_1_SUBSET_",i,".Rds")))
+  ## Delete existing subsets
+  files_to_rm <- file.path(osm_dir, "FinalData", osm_country_dir) %>%
+    list.files() %>%
+    str_subset("_SUBSET_")
+  
+  for(file_to_rm_i in files_to_rm){
+    file.path(osm_dir, "FinalData", osm_country_dir, file_to_rm_i) %>%
+      file.remove()
+  }
+  
+  ## Load data
+  osm_df <- readRDS(file.path(osm_dir, "FinalData", 
+                              osm_country_dir, "gis_osm_roads_free_1.Rds"))
+  
+  ## Determine subsets
+  group_vec <- rep_len(x = 1:N_SUBSETS, length.out = nrow(osm_df))
+  
+  ## Save into different subsets
+  for(i in 1:N_SUBSETS){
+    print(i)
+    
+    osm_df_i <- osm_df[group_vec %in% i,]
+    
+    saveRDS(osm_df_i,
+            file.path(osm_dir, "FinalData", 
+                      osm_country_dir, paste0("gis_osm_roads_free_1_SUBSET_",i,".Rds")))
+  }
+  
 }
+
+
+
 
 
 
--- DataWork/02_get_process_ancillary_data/OSM/03_extract_road_distance.R
+++ DataWork/02_get_process_ancillary_data/OSM/03_extract_road_distance.R
@@ -1,26 +1,38 @@
 # Extract road density and distance to nearest road for each survey location
 # and road type
 
-buffer_size_osm_m <- c(5000)
-SURVEY_NAME <- "DHS"
-
 RE_EXTRACT_IF_EXISTS <- F
 
 # Functions ====================================================================
-load_prep_osm_roads <- function(country_code, osm_dir_df, cc_epsg){
+load_prep_osm_roads <- function(country_code, 
+                                osm_dir_df, 
+                                cc_epsg,
+                                data_split_into_subsets,
+                                subset_id = NULL,
+                                project = TRUE){
   # DESCRIPTION: Load and Prep OSM Road Data
   # ARGS
   # -- country_code: Two letter country code
   # -- osm_dir_df: Dataframe with country codes and OSM directories
   # -- cc_epsg: Projection
+  # -- data_split_into_subsets: If the dataset is split into segments
+  # -- subset_id: If the data is split into subsets, what is the subset_id?
   
   ### A. Define directory
   osm_country_dir <- osm_dir_df$osm_dirs[osm_dir_df$country_code %in% country_code]
   
   ### B. Load data
-  osm_df <- readRDS(file.path(osm_dir, "FinalData", osm_country_dir, "gis_osm_roads_free_1.Rds"))
+  print("Load OSM data")
+  if(data_split_into_subsets){
+    osm_df <- readRDS(file.path(osm_dir, "FinalData", osm_country_dir, 
+                                paste0("gis_osm_roads_free_1_SUBSET_",subset_id,".Rds")))
+  } else{
+    osm_df <- readRDS(file.path(osm_dir, "FinalData", osm_country_dir, 
+                                "gis_osm_roads_free_1.Rds"))
+  }
   
   ### C. Cleanup
+  print("Cleanup OSM data")
   osm_df$fclass <- osm_df$fclass %>% as.character()
   osm_df$fclass[grepl("trunk", osm_df$fclass)]     <- "trunk"
   osm_df$fclass[grepl("motorway", osm_df$fclass)]  <- "motorway"
@@ -30,15 +42,22 @@ load_prep_osm_roads <- function(country_code, osm_dir_df, cc_epsg){
   osm_df$fclass[grepl("track", osm_df$fclass)]     <- "track"
   
   ### D. Remove certain classes
+  print("Subset OSM data")
   osm_df <- osm_df[!(osm_df$fclass %in% c("steps",
                                           "unknown",
                                           "bridleway",
                                           "cycleway")),]
   
+  print(table(osm_df$fclass))
+  
   ### E. Project
-  osm_df <- osm_df %>% spTransform(CRS(cc_epsg))
+  if(project){
+    print("Project OSM data")
+    osm_df <- osm_df %>% spTransform(CRS(cc_epsg))
+  }
   
   ### F. To SF
+  print("OSM data to SF")
   osm_sf <- osm_df %>% st_as_sf()
   
   return(osm_sf)
@@ -113,6 +132,10 @@ survey_df <- survey_df %>%
   dplyr::filter(!is.na(latitude)) %>%
   dplyr::mutate(uid = uid %>% as.character)
 
+coordinates(survey_df) <- ~longitude+latitude
+crs(survey_df) <- CRS("+init=epsg:4326")
+survey_sf <- survey_df %>% st_as_sf()
+
 country_codes_all <- survey_df$country_code %>% unique()
 
 # 2. Load country_code to OSM dir data -----------------------------------------
@@ -143,50 +166,73 @@ osm_dir_df <- osm_dir_df %>%
   dplyr::filter(!is.na(country_code))
 
 # 3. Extract density -----------------------------------------------------------
-buffer_m = 5000
-country_code <- "TZ"
-
-for(country_code in country_codes_all){
+for(country_code in "IA"){ # country_codes_all
   
-  OUT_PATH <- file.path(data_dir, SURVEY_NAME, "FinalData", 
-                        "Individual Datasets", "osm", "roads_density",
-                        paste0("osm_roaddensity_", buffer_m, "m_", country_code, ".Rds"))
-  print(OUT_PATH)
+  #### Load road files 
+  # Sometimes we split road files into subsets. So either load the subsets or 
+  # just the full road file
+  osm_country_dir <- osm_dir_df$osm_dirs[osm_dir_df$country_code %in% country_code] 
   
-  if(RE_EXTRACT_IF_EXISTS | !file.exists(OUT_PATH)){
+  osm_country_road_files <- file.path(osm_dir, "FinalData", osm_country_dir) %>%
+    list.files() %>%
+    str_subset("gis_osm_roads_free")
+  
+  # If contains subsets, only use the subsets
+  if(TRUE %in% grepl("SUBSET", osm_country_road_files)){
+    osm_country_road_files <- osm_country_road_files[grepl("SUBSET", osm_country_road_files)]
     
-    #### Grab projection for country i
-    epsg_num <- survey_proj_df$epsg_projection[survey_proj_df$country_code %in% country_code]
-    cc_epsg <- paste0("+init=epsg:",epsg_num)
+    subset_id <- osm_country_road_files %>%
+      str_replace_all("gis_osm_roads_free_1_SUBSET_", "") %>%
+      str_replace_all(".Rds", "")
+    data_split_into_subsets <- T
+  } else{
+    subset_id <- "1"
+    data_split_into_subsets <- F
+  }
+  
+  #### Loop through road files (either full road file or all the subsets)
+  for(subset_id_i in subset_id){
     
-    #### Load OSM
-    osm_sf <- load_prep_osm_roads(country_code, osm_dir_df, cc_epsg)
+    OUT_PATH <- file.path(data_dir, SURVEY_NAME, "FinalData", 
+                          "Individual Datasets", "osm", "roads_distance",
+                          paste0("osm_roaddistance_", buffer_m, "m_", country_code, "_subset_",subset_id_i,".Rds"))
+    print(OUT_PATH)
     
-    #### Prep survey data
-    # Subset and buffer
-    survey_df <- survey_df[survey_df$country_code %in% country_code,]
+    if(RE_EXTRACT_IF_EXISTS | !file.exists(OUT_PATH)){
+      
+      #### Load OSM
+      osm_sf <- load_prep_osm_roads(country_code,
+                                    osm_dir_df, 
+                                    cc_epsg = "BLANK",
+                                    data_split_into_subsets,
+                                    subset_id_i,
+                                    project = F)
+      
+      #### Load Survey
+      survey_sf_i <- survey_sf[survey_sf$country_code %in% country_code,]
+      
+      #### Extract density
+      osm_distance_country_i <- lapply(unique(osm_sf$fclass), function(fclass_i){
+        print(fclass_i)
+        
+        osm_sf_i <- osm_sf[osm_sf$fclass %in% fclass_i,]
+        
+        osm_nearest_id <- st_nearest_feature_chunks(survey_sf_i, osm_sf_i, 1000)
+        osm_nearest_sf_i <- osm_sf_i[osm_nearest_id,]
+        
+        dist <- st_distance(survey_sf_i, osm_nearest_sf_i, by_element = T)
+        
+        out_df <- data.frame(uid = survey_sf_i$uid,
+                             dist = as.numeric(dist))
+        names(out_df)[names(out_df) %in% "dist"] <- paste0("osm_dist_", fclass_i)
+        
+        return(out_df)
+      }) %>%
+        reduce(left_join, by = "uid")
     
-    survey_df <- survey_df %>%
-      dplyr::select(uid, latitude, longitude)
-    
-    coordinates(survey_df) <- ~longitude+latitude
-    crs(survey_df) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "
-    
-    survey_buff_df <- geo.buffer_chunks(survey_df, r = buffer_m, chunk_size = 100)
-    survey_buff_df <- survey_buff_df %>% spTransform(CRS(cc_epsg))
-    
-    survey_buff_sf <- survey_buff_df %>% st_as_sf()
-    survey_buff_sf_agg <- survey_buff_sf %>% st_union()
-    
-    #### Extract density
-    osm_density_country_i <- lapply(unique(osm_sf$fclass),
-                                    extract_density_fclass_i,
-                                    osm_sf, 
-                                    survey_buff_sf, 
-                                    survey_buff_sf_agg) %>%
-      reduce(left_join, by = "uid")
-    
-    saveRDS(osm_density_country_i, OUT_PATH)
+      #### Export
+      saveRDS(osm_distance_country_i, OUT_PATH)
+    }
   }
 }
 

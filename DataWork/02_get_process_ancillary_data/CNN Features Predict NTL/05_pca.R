7726a61c808307e54c805ca521c0e9b889f54a49
Rob Marty <ramarty@email.wm.edu>
2021-10-11 10:33:20 -0400

    pca

--- DataWork/02_get_process_ancillary_data/CNN Features Predict NTL/05_pca.R
+++ DataWork/02_get_process_ancillary_data/CNN Features Predict NTL/05_pca.R
@@ -10,6 +10,20 @@ cnn_feat_rgb <- fread(file.path(data_dir,
                                 'cnn_features',
                                 paste0('cnn_features_', SATELLITE, '_rgb.csv')))
 
+cnn_feat_noid <- cnn_feat_rgb %>%
+  dplyr::select(-uid) %>%
+  as.data.frame()
+
+cnn_feat_sd <- cnn_feat_noid %>%
+  apply(2, sd) %>%
+  as.numeric()
+
+cnn_feat_noid <- cnn_feat_noid[,cnn_feat_sd > 0]
+
+pca_obj <- cnn_feat_noid %>%
+  prcomp(scale = T)
+
+
 #### PCA HERE
 
 a <- cnn_feat_rgb %>%

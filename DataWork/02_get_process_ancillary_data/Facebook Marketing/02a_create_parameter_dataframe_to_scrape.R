be3478c6d33ab79feeb42940aa701219f23caed5
Rob Marty <ramarty@email.wm.edu>
2021-10-15 17:56:25 -0400

    fb

---
+++ DataWork/02_get_process_ancillary_data/Facebook Marketing/02a_create_parameter_dataframe_to_scrape.R
@@ -0,0 +1,158 @@
+# Create Parameter Dataframe to Scrape
+
+# Parameters -------------------------------------------------------------------
+AGE_MIN = 18
+AGE_MAX = 65
+FACEBOOK_POSITIONS = "'feed','instant_article','instream_video','marketplace'"
+RADIUS_KM = 10
+
+# All
+parameters_df_all <- data.frame(
+  radius_km = RADIUS_KM,
+  gender = "1,2",
+  age_min = AGE_MIN,
+  age_max = AGE_MAX,
+  facebook_positions = FACEBOOK_POSITIONS,
+  device_platforms = "'mobile','desktop'",
+  publisher_platforms = "'facebook','messenger'",
+  messenger_positions = "'messenger_home'",
+  stringsAsFactors = F
+)
+
+# By education status
+parameters_df_educ <- data.frame(
+  radius_km = RADIUS_KM,
+  gender = "1,2",
+  age_min = AGE_MIN,
+  age_max = AGE_MAX,
+  facebook_positions = FACEBOOK_POSITIONS,
+  device_platforms = "'mobile','desktop'",
+  publisher_platforms = "'facebook','messenger'",
+  messenger_positions = "'messenger_home'",
+  education_statuses = c("1,4,13",
+                         "2,5,6,7,8,9,10,11"),
+  stringsAsFactors = F
+)
+
+# By device type
+parameters_df_device <- data.frame(
+  radius_km = RADIUS_KM,
+  gender = "1,2",
+  age_min = AGE_MIN,
+  age_max = AGE_MAX,
+  facebook_positions = FACEBOOK_POSITIONS,
+  device_platforms = "'mobile','desktop'",
+  publisher_platforms = "'facebook','messenger'",
+  messenger_positions = "'messenger_home'",
+  user_os = c("iOS",
+              "Android",
+              #"'iOS_ver_2.0_to_3.0'",
+              #"'iOS_ver_4.0_to_5.0'",
+              #"'iOS_ver_6.0_to_7.0'",
+              #"'iOS_ver_8.0_and_above'",
+              "iOS_ver_9.0_and_above",
+              #"'Android_ver_2.0_to_3.2'",
+              #"'Android_ver_4.0_to_5.1'",
+              #"'Android_ver_6.0_to_7.1'",
+              #"'Android_ver_7.0_and_above'",
+              "Android_ver_8.0_and_above"),
+  stringsAsFactors = F
+)
+
+# Wireless carrier
+parameters_df_carrier <- data.frame(
+  radius_km = RADIUS_KM,
+  gender = "1,2",
+  age_min = AGE_MIN,
+  age_max = AGE_MAX,
+  facebook_positions = FACEBOOK_POSITIONS,
+  device_platforms = "'mobile','desktop'",
+  publisher_platforms = "'facebook','messenger'",
+  messenger_positions = "'messenger_home'",
+  wireless_carrier = "'wifi'",
+  stringsAsFactors = F
+)
+
+# Behaviors
+parameters_df_behaviors <- data.frame(
+  radius_km = RADIUS_KM,
+  gender = "1,2",
+  age_min = AGE_MIN,
+  age_max = AGE_MAX,
+  facebook_positions = FACEBOOK_POSITIONS,
+  device_platforms = "'mobile','desktop'",
+  publisher_platforms = "'facebook','messenger'",
+  messenger_positions = "'messenger_home'",
+  behavior = c(
+    # Network access
+    "6017253486583", # Facebook access (network type): 2G
+    "6017253511583", # Facebook access (network type): 3G
+    "6017253531383", # Facebook access (network type): 4G
+    "6015235495383", # Facebook access (network type): WiFi
+    
+    # Mobile OS
+    "6004386044572", # Facebook access (mobile): Android devices
+    "6004384041172", # Facebook access (mobile): Apple (iOS) devices
+    "6004385895772", # Facebook access (mobile): Windows phones
+    
+    # High end phones
+    "6092512462983,6120699687383,6120699721983,6120699725783", # Facebook access (mobile): iPhone X/XS/XS Max/XR
+    "6092512462983,6120699687383,6120699721983,6120699725783,6092512412783,6092512424583", # iPhone X/XS/XS Max/XR/8/8 Plus
+    "6106224431383", # Owns: Galaxy S9+
+    "6075237200983,6075237226583,6106223987983,6106224431383", #Samsung Galaxy phone S8/S8+/S9/S9+ 
+    "6075237200983,6075237226583,6106223987983,6106224431383,6092512462983,6120699687383,6120699721983,6120699725783,6092512412783,6092512424583", # iPhone X/XS/XS Max/XR/8/8 Plus or Samsung Galaxy phone S8/S8+/S9/S9+ 
+    
+    # Other device types
+    "6004382299972", # Facebook access (mobile): all mobile devices
+    "6004383149972", # Facebook access (mobile): feature phones
+    "6004383049972", # Facebook access (mobile): smartphones and tablets
+    "6016286626383", # Facebook access (mobile): tablets
+    "6023460590583", # Owns: Cherry Mobile
+    "6056265212183", # Owns: VIVO devices
+    "6011390261383", # Owns: Huawei
+    "6056265200983", # Owns: Oppo
+    "6056265200983,6056265212183,6023460590583", # Oppo/Vivo/Cherry
+    "6004386010572", # Facebook access (mobile): Samsung Android mobile devices
+    
+    # OTHER
+    "6002714895372", # Frequent Travelers
+    "6022788483583", # Frequent international travelers
+    "6002714898572", # Small business owners
+    "6003808923172", # Technology early adopters
+    "6004854404172" # Facebook access: older devices and OS
+  ), 
+  stringsAsFactors = F
+)
+
+# Interests
+parameters_df_interests <- data.frame(
+  radius_km = RADIUS_KM,
+  gender = "1,2",
+  age_min = AGE_MIN,
+  age_max = AGE_MAX,
+  facebook_positions = FACEBOOK_POSITIONS,
+  device_platforms = "'mobile','desktop'",
+  publisher_platforms = "'facebook','messenger'",
+  messenger_positions = "'messenger_home'",
+  interest = c("6003012317397", # Gambling
+               "6003384248805", # Fitness and wellness
+               "6007828099136", # Luxury Goods
+               "6002868910910", # Organic food
+               "6004037400009" # Fast food
+  ), 
+  stringsAsFactors = F
+)
+
+# Append
+parameters_df <- bind_rows(
+  parameters_df_all,
+  parameters_df_educ,
+  parameters_df_behaviors,
+  parameters_df_interests
+)
+
+parameters_df$param_id <- 1:nrow(parameters_df)
+
+# Export -----------------------------------------------------------------------
+saveRDS(parameters_df, file.path(data_dir, "Facebook Marketing", "FinalData", "facebook_marketing_parameters.Rds"))
+
--- DataWork/02_get_process_ancillary_data/Facebook Marketing/04_append_data.R
+++ DataWork/02_get_process_ancillary_data/Facebook Marketing/04_append_data.R
@@ -42,29 +42,20 @@ df_wide <- df_long %>%
 # estimate_[mau/dau]_1 is all facebook users, so divide by this to 
 # get proportion of facebook users that meet a certain category
 
-df_wide_prop <- df_wide %>%
-  dplyr::mutate_at(vars(contains("dau"), -estimate_dau_1), ~(. /  estimate_dau_1)) %>%
-  dplyr::mutate_at(vars(contains("mau"), -estimate_mau_1), ~(. /  estimate_mau_1)) %>%
-  dplyr::select(-c(estimate_dau_1, estimate_mau_1)) %>%
-  dplyr::mutate_if(is.numeric, tidyr::replace_na, 0)
+# df_wide_prop <- df_wide %>%
+#   dplyr::mutate_at(vars(contains("dau"), -estimate_dau_1), ~(. /  estimate_dau_1)) %>%
+#   dplyr::mutate_at(vars(contains("mau"), -estimate_mau_1), ~(. /  estimate_mau_1)) %>%
+#   dplyr::select(-c(estimate_dau_1, estimate_mau_1)) %>%
+#   dplyr::mutate_if(is.numeric, tidyr::replace_na, 0)
 
 # Export -----------------------------------------------------------------------
 saveRDS(df_wide, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau.Rds"))
-write.csv(df_wide, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau.csv"), row.names = F)
+#write.csv(df_wide, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau.csv"), row.names = F)
 
-saveRDS(df_wide_prop, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau_prop.Rds"))
-write.csv(df_wide_prop, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau_prop.csv"), row.names = F)
+#saveRDS(df_wide_prop, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau_prop.Rds"))
+#write.csv(df_wide_prop, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_dau_mau_prop.csv"), row.names = F)
 
-# Indicates parameters for each "param_version"
-if(F){
-  param_df <- df_long %>%
-    dplyr::select(-c(estimate_dau, estimate_mau, latitude, longitude, api_call_time_utc)) %>%
-    dplyr::distinct(param_id, .keep_all = T) %>%
-    dplyr::select(param_id, everything())
-  
-  saveRDS(param_df, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_parameters.Rds"))
-  write.csv(param_df, file.path(data_dir, SURVEY_NAME, "FinalData", "Individual Datasets", "facebook_marketing_parameters.csv"), row.names = F)
-}
+
 
 
 

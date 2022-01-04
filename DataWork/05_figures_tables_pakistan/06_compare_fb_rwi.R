cf093a98304c3835f9b92f9b077ba139edb589c7
Rob Marty <ramarty@email.wm.edu>
2022-01-04 15:00:28 -0500

    up

--- DataWork/05_figures_tables_pakistan/06_compare_fb_rwi.R
+++ DataWork/05_figures_tables_pakistan/06_compare_fb_rwi.R
@@ -24,21 +24,19 @@ cor_ur_df <- dhs_df %>%
   dplyr::summarise(r2_estimate = cor(wealth_index_score, predict_wealth_index_score_best)^2,
                    r2_fbrwi = cor(wealth_index_score, fb_rwi)^2,
                    wealth_index_score_sd = sd(wealth_index_score),
-                   wealth_index_score_mean = mean(wealth_index_score))
+                   wealth_index_score_mean = mean(wealth_index_score),
+                   N = n())
 
 cor_adm2_df <- dhs_df %>%
   group_by(NAME_1) %>%
   dplyr::summarise(r2_estimate = cor(wealth_index_score, predict_wealth_index_score_best)^2,
                    r2_fbrwi = cor(wealth_index_score, fb_rwi)^2,
                    wealth_index_score_sd = sd(wealth_index_score),
-                   wealth_index_score_mean = mean(wealth_index_score))
+                   wealth_index_score_mean = mean(wealth_index_score),
+                   N = n())
 
 cor_ur_df
 cor_df
 cor_adm2_df
 
-cor_adm2_df %>%
-ggplot() +
-  geom_point(aes(x = r2_estimate,
-                 y = wealth_index_score_sd))
 

e6284f8fd6edfb4f1e474aeff65fdb599c1e9dc3
Rob Marty <ramarty@email.wm.edu>
2022-06-24 16:04:09 -0400

    process cnn data

--- DataWork/02_get_process_ancillary_data/CNN Features Predict NTL/06_data_to_db.R
+++ DataWork/02_get_process_ancillary_data/CNN Features Predict NTL/06_data_to_db.R
@@ -1,46 +1,57 @@
 # Move data from Google Drive to Dropbox
 
-SATELLITE <- 's2'
-BANDS <- "rgb"
+SATELLITE <- "viirs"
+UNDER_IA  <- "False"
+BANDS     <- "rgb"
 
-for(BANDS in c("rgb", "ndvi", "bu")){
-  
-  print(paste(BANDS, "-------------------------------------------------------"))
-  
-  # Features -------------------------------------------------------------------
-  cnn_features_df <- fread(file.path(gdrive_dir,
-                                     "Data",
-                                     SURVEY_NAME,
-                                     "FinalData",
-                                     "cnn_features",
-                                     paste0('features_', SATELLITE, '_b_',BANDS,'.csv')))
-  
-  cnn_features_df <- cnn_features_df %>% as.data.frame()
-  
-  saveRDS(cnn_features_df,
-          file.path(data_dir,
-                    SURVEY_NAME,
-                    "FinalData",
-                    "Individual Datasets",
-                    "cnn_features",
-                    paste0("cnn_features_", SATELLITE, "_", BANDS, ".Rds")))
-  
-  # Predictions ----------------------------------------------------------------
-  cnn_predictions_df <- fread(file.path(gdrive_dir,
-                                        "Data",
-                                        SURVEY_NAME,
-                                        "FinalData",
-                                        "cnn_predictions",
-                                        paste0('predictions_', SATELLITE, '_b_',BANDS,'.csv')))
-  
-  cnn_predictions_df$uid <- cnn_predictions_df$uid %>%  str_replace_all("b'", "") %>% str_replace_all("'", "")
-  
-  saveRDS(cnn_predictions_df %>% as.data.frame(),
-          file.path(data_dir,
-                    SURVEY_NAME,
-                    "FinalData",
-                    "Individual Datasets",
-                    "cnn_features",
-                    paste0("cnn_predictions_", SATELLITE, "_", BANDS, ".Rds")))
-  
+for(SATELLITE in c("viirs")){
+  for(UNDER_IA in c("False")){
+    for(BANDS in c("rgb", "ndvi", "bu")){
+      
+      PATH_NAME <- paste0("landsat_",SATELLITE,"_underia",UNDER_IA,"_",BANDS)
+      
+      print(paste(PATH_NAME, "-------------------------------------------------------"))
+      
+      # Features -------------------------------------------------------------------
+      # In Google Colab, RAM maxed out trying to process full dataset at once, so 
+      # outputted multiple datasets. Here we append them.
+      cnn_features_df <- file.path(gdrive_dir,
+                                   "Data",
+                                   SURVEY_NAME,
+                                   "FinalData",
+                                   "cnn_features") %>%
+        list.files(full.names = T) %>%
+        str_subset(paste0("features", PATH_NAME, "_")) %>%
+        map_df(fread)
+      
+      cnn_features_df <- cnn_features_df %>% as.data.frame()
+      
+      saveRDS(cnn_features_df,
+              file.path(data_dir,
+                        SURVEY_NAME,
+                        "FinalData",
+                        "Individual Datasets",
+                        "cnn_features",
+                        paste0("cnn_features_", PATH_NAME, ".Rds")))
+      
+      # Predictions ----------------------------------------------------------------
+      cnn_predictions_df <- fread(file.path(gdrive_dir,
+                                            "Data",
+                                            SURVEY_NAME,
+                                            "FinalData",
+                                            "cnn_predictions",
+                                            paste0('predictions_', PATH_NAME,'.csv')))
+      
+      cnn_predictions_df$uid <- cnn_predictions_df$uid %>%  str_replace_all("b'", "") %>% str_replace_all("'", "")
+      
+      saveRDS(cnn_predictions_df %>% as.data.frame(),
+              file.path(data_dir,
+                        SURVEY_NAME,
+                        "FinalData",
+                        "Individual Datasets",
+                        "cnn_features",
+                        paste0("cnn_predictions_", PATH_NAME, ".Rds")))
+      
+    }
+  }
 }
--- DataWork/02_get_process_ancillary_data/CNN Features Predict NTL/07_pca.R
+++ DataWork/02_get_process_ancillary_data/CNN Features Predict NTL/07_pca.R
@@ -1,55 +1,62 @@
 # PCA on CNN Features
 
-SATELLITE <- 's2'
+SATELLITE <- 'viirs'
 BANDS <- "rgb"
+UNDER_IA <- "False"
 PER_VAR_EXPLAIN <- 0.9
 
-for(BANDS in c("rgb", "ndvi", "bu")){
-  
-  # Load data --------------------------------------------------------------------
-  cnn_feat_rgb <- readRDS(file.path(data_dir,
-                                    SURVEY_NAME,
-                                    "FinalData",
-                                    "Individual Datasets",
-                                    "cnn_features",
-                                    paste0("cnn_features_", SATELLITE, "_", BANDS, ".Rds")))
-  
-  ## Grab features (remove ID)
-  cnn_feat_noid <- cnn_feat_rgb %>%
-    dplyr::select(-uid) %>%
-    as.data.frame()
-  
-  ## Grab features that are non-constant
-  cnn_feat_sd <- cnn_feat_noid %>%
-    apply(2, sd) %>%
-    as.numeric()
-  
-  cnn_feat_noid <- cnn_feat_noid[,cnn_feat_sd > 0]
-  
-  ## PCA and grab features that explain 90% of variation
-  pca_obj <- cnn_feat_noid %>%
-    prcomp(scale = T)
-  
-  # Objsect with % variation explained
-  pca_obj_sum <- summary(pca_obj) 
-  
-  use_features_tf <- as.numeric(pca_obj_sum$importance[3,]) < PER_VAR_EXPLAIN
-  
-  name_prefix <- paste0("cnn_" ,SATELLITE, "_", BANDS, "_")
-  pca_df <- pca_obj$x[,use_features_tf] %>%
-    as.data.frame() %>%
-    rename_all(~tolower(paste0(name_prefix, .)))
-  
-  ## Add UID back in
-  pca_df$uid <- cnn_feat_rgb$uid %>% str_replace_all("b'", "") %>% str_replace_all("'", "")
-  
-  # Export -----------------------------------------------------------------------
-  saveRDS(pca_df,
-          (file.path(data_dir,
-                     SURVEY_NAME,
-                     'FinalData',
-                     'Individual Datasets',
-                     'cnn_features',
-                     paste0('cnn_features_', SATELLITE, '_',BANDS,'_pca.Rds'))))
-  
+for(SATELLITE in c("viirs")){
+  for(UNDER_IA in c("False")){
+    for(BANDS in c("rgb", "ndvi", "bu")){
+      
+      PATH_NAME <- paste0("landsat_",SATELLITE,"_underia",UNDER_IA,"_",BANDS)
+      
+      # Load data --------------------------------------------------------------------
+      cnn_feat_rgb <- readRDS(file.path(data_dir,
+                                        SURVEY_NAME,
+                                        "FinalData",
+                                        "Individual Datasets",
+                                        "cnn_features",
+                                        paste0("cnn_features_", PATH_NAME, ".Rds")))
+      
+      ## Grab features (remove ID)
+      cnn_feat_noid <- cnn_feat_rgb %>%
+        dplyr::select(-uid) %>%
+        as.data.frame()
+      
+      ## Grab features that are non-constant
+      cnn_feat_sd <- cnn_feat_noid %>%
+        apply(2, sd) %>%
+        as.numeric()
+      
+      cnn_feat_noid <- cnn_feat_noid[,cnn_feat_sd > 0]
+      
+      ## PCA and grab features that explain 90% of variation
+      pca_obj <- cnn_feat_noid %>%
+        prcomp(scale = T)
+      
+      # Objsect with % variation explained
+      pca_obj_sum <- summary(pca_obj) 
+      
+      use_features_tf <- as.numeric(pca_obj_sum$importance[3,]) < PER_VAR_EXPLAIN
+      
+      name_prefix <- paste0("cnn_" ,SATELLITE, "_", BANDS, "_")
+      pca_df <- pca_obj$x[,use_features_tf] %>%
+        as.data.frame() %>%
+        rename_all(~tolower(paste0(name_prefix, .)))
+      
+      ## Add UID back in
+      pca_df$uid <- cnn_feat_rgb$uid %>% str_replace_all("b'", "") %>% str_replace_all("'", "")
+      
+      # Export -----------------------------------------------------------------------
+      saveRDS(pca_df,
+              (file.path(data_dir,
+                         SURVEY_NAME,
+                         'FinalData',
+                         'Individual Datasets',
+                         'cnn_features',
+                         paste0('cnn_features_', PATH_NAME,'_pca.Rds'))))
+      
+    }
+  }
 }

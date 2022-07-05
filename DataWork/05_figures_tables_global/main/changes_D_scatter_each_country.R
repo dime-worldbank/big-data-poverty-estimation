bc0634e2520069e63f5bebc9891804eac3c65205
Rob Marty <ramarty@email.wm.edu>
2022-07-05 19:07:25 -0400

    si figures

--- DataWork/05_figures_tables_global/main/changes_D_scatter_each_country.R
+++ DataWork/05_figures_tables_global/main/changes_D_scatter_each_country.R
@@ -11,13 +11,13 @@ survey_df <- readRDS(file.path(data_dir, SURVEY_NAME, "FinalData", "Merged Datas
 p_pca <- survey_df %>%
   group_by(country_name) %>%
   dplyr::mutate(r2 = cor(pca_allvars,
-                         predict_pca_allvars_best)^2) %>%
+                         predict_pca_allvars_within_country_cv_all_changes)^2) %>%
   ungroup() %>%
   dplyr::mutate(country_name = fct_reorder(country_name,
                                            -r2)) %>%
   ggplot(aes(x = pca_allvars,
-             y = predict_pca_allvars_best)) +
-  geom_point() +
+             y = predict_pca_allvars_within_country_cv_all_changes)) +
+  geom_point(size = 0.3) +
   stat_poly_eq(color = "firebrick3") +
   facet_wrap(~country_name) +
   theme_classic() +
@@ -29,13 +29,13 @@ p_pca <- survey_df %>%
 p_dhs <- survey_df %>%
   group_by(country_name) %>%
   dplyr::mutate(r2 = cor(wealth_index_score,
-                         predict_wealth_index_score_best)^2) %>%
+                         predict_wealth_index_score_within_country_cv_all_changes)^2) %>%
   ungroup() %>%
   dplyr::mutate(country_name = fct_reorder(country_name,
                                            -r2)) %>%
   ggplot(aes(x = wealth_index_score,
-             y = predict_wealth_index_score_best)) +
-  geom_point() +
+             y = predict_wealth_index_score_within_country_cv_all_changes)) +
+  geom_point(size = 0.3) +
   stat_poly_eq(color = "firebrick3") +
   facet_wrap(~country_name) +
   theme_classic() +
